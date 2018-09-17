a2318ecb05b96f9eaa22d82f06b537e2bcbd43e1
doomcrewinc <oilfield101@gmail.com>
2018-09-16 18:01:42 -0600

    Updated to discord.py[rewrite]

--- catfacts/catfacts.py
+++ catfacts/catfacts.py
@@ -1,32 +1,30 @@
-# Developed by doomcrewinc
+# Developed by doomcrewinc for OdinForce
 from discord.ext import commands
 import random
 import json
 from pprint import pprint
+
 
 class Catfact:
     """Random Cat Fact."""
 
     def __init__(self, bot):
         self.bot = bot
-        
-        with open('catfacts.json', encoding='utf-8') as cat_file:
-            catdata = json.loads(cat_file.read())
-        factslist=[]
-        for line in catdata["data"]:
-            factslist.append(line)
-        choice=random.choice(factslist)
-        pprint(choice['fact'])
-        
-        
 
     @commands.command(name="catfact")
-    async def _cookie(self):
+    async def _cookie(self, ctx):
         """You want a catfact?
 
         Here's your catfact!
         """
-        return await self.bot.say("`" + choice['fact'] + "`")
+        with open('catfacts.json', encoding='utf-8') as cat_file:
+            catdata = json.loads(cat_file.read())
+        factslist = []
+        for line in catdata["data"]:
+            factslist.append(line)
+        choice = random.choice(factslist)
+        pprint(choice['fact'])  # Echos the fact to the console, totally unnecessary but I leave it enabled.
+        await ctx.send("`" + choice['fact'] + "`")
 
 
 def setup(bot):
